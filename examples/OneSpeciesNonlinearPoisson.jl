6007a5a9e107e9ae0542671f985ad4cab98b2757
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2019-04-09 23:21:19 +0200

    updated some documentation

--- examples/OneSpeciesNonlinearPoisson.jl
+++ examples/OneSpeciesNonlinearPoisson.jl
@@ -39,63 +39,87 @@ end
 # for testimg. Default physics need to generate correct
 # test value.
 
-
 function main(;n=10,pyplot=false,verbose=false, dense=false)
 
 
-    
+
+    # Create a one dimensional discretization project
     h=1.0/convert(Float64,n)
     grid=VoronoiFVM.Grid(collect(0:h:1))
     
-    
+    # Create a physics structure
     physics=Physics()
     physics.eps=1.0e-2
 
+    # Flux function which describes the flux
+    # between neigboring control volumes
     physics.flux=function(physics,edge,f,uk,ul)
         f[1]=physics.eps*(uk[1]^2-ul[1]^2)
     end 
 
+
+    # Source term
     physics.source=function(physics,node,f)
         f[1]=1.0e-4*node.coord[1]
     end 
 
+    # Storage term (under the time derivative)
     physics.storage=function(physics,node, f,u)
         f[1]=u[1]
     end
 
+    # Reation term
     physics.reaction=function(physics,node,f,u)
         f[1]=u[1]^2
-
     end
 
+
+    # Create a finite volume system - either
+    # in the dense or  the sparse version.
+    # Need to provide the overall number of species here
     if dense
         sys=VoronoiFVM.DenseSystem(grid,physics,1)
     else
         sys=VoronoiFVM.SparseSystem(grid,physics,1)
     end
+
+    # Add species 1 to region 1
     add_species(sys,1,[1])
+
+    # Set boundary conditions
     sys.boundary_values[1,1]=1.0
     sys.boundary_values[1,2]=0.5
-    
     sys.boundary_factors[1,1]=VoronoiFVM.Dirichlet
     sys.boundary_factors[1,2]=VoronoiFVM.Dirichlet
-    
+
+    # Create a solution array
     inival=unknowns(sys)
+
+    # Broadcast the initial value
     inival.=0.5
 
-
+    # Create solver control info
     control=VoronoiFVM.NewtonControl()
     control.verbose=verbose
+
+    # time stepping
     tstep=1.0e-2
     times=collect(0.0:tstep:1.0)
     test_result=0
     for it=2:length(times)
+        # Solve for new timestep with old timestep
+        # solution in inival
         U=solve(sys,inival,control=control,tstep=tstep)
         test_result=U[5]
+
+        # Update inival
         inival.=U
+
         if verbose
             @printf("time=%g\n",times[it])
         end
+
+        # Plot data
         if pyplot
             PyPlot.clf()
             PyPlot.grid()
