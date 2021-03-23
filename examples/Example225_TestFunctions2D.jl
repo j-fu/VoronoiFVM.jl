1e3cbcd96b769712c8716e6dc6bde00173cbe0ae
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2021-03-23 10:53:45 +0100

    try to fix ci on windows

--- examples/Example225_TestFunctions2D.jl
+++ examples/Example225_TestFunctions2D.jl
@@ -57,8 +57,8 @@ Let $T(x)$ be the solution of the Laplace problem $-\nabla^2 T =0$ in $\Omega$ a
 
 ```math
 \begin{aligned}
-  T &=0 \quad \text{at}\; \Gamma_4\\
-  T &=1 \quad \text{at}\; \Gamma_2\\
+T &=0 \quad \text{at}\; \Gamma_4\\
+T &=1 \quad \text{at}\; \Gamma_2\\
 \partial_n T &=0\quad \text{at}\;  \Gamma_1,\Gamma_3
 \end{aligned}
 ```
@@ -70,9 +70,9 @@ Write ``\vec j=-\nabla u``. and assume $\nabla\cdot \vec j + r =f$.
 \begin{aligned}
 \int_{\Gamma_2} \vec j \cdot \vec n ds&=\int_{\Gamma_2} T\vec j \cdot \vec n ds \quad \text{due to}\; T=1\; \text{on}\; \Gamma_2\\
  &=\int_{\partial\Omega}  T\vec j \cdot \vec n ds\quad \text{due to}\; T=0\; \text{on}\; \Gamma_4, \quad\vec j\cdot \vec n=0\; \text{on}\; \Gamma_1, \Gamma_3\\ 
- &= \int_\Omega \nabla \cdot (T \vec j) d\omega \quad \text{(Gauss)}\\
- &= \int_\Omega \nabla T \cdot \vec j d\omega + \int_\Omega T \nabla\cdot j d\omega\\
- &=  \int_\Omega \nabla T \cdot \vec j d\omega + \int_\Omega T(f-r)dω\\
+&= \int_\Omega \nabla \cdot (T \vec j) d\omega \quad \text{(Gauss)}\\
+&= \int_\Omega \nabla T \cdot \vec j d\omega + \int_\Omega T \nabla\cdot j d\omega\\
+&=  \int_\Omega \nabla T \cdot \vec j d\omega + \int_\Omega T(f-r)dω\\
 \end{aligned}
 ```
 
@@ -107,7 +107,7 @@ Literature references:
 
 - H. Gajewski "Analysis und Numerik von Ladungstransport in Halbleitern", WIAS Berlin, Report No.6
 - Yoder, P. D., K. Gärtner, and W. Fichtner. "A generalized Ramo–Shockley theorem for classical to quantum transport at arbitrary frequencies." Journal of Applied Physics 79.4 (1996): 1951-1954.
-- P. Farrell, N. Rotundo, D. H. Doan, M. Kantner, J. Fuhrmann, and T. Koprucki, "Numerical methods for drift-diffusion models", in Handbook of optoelectronic device modeling and simulation: Lasers, modulators, photodetectors, solar cells, and numerical methods, vol. 2, J. Piprek, Ed. Boca Raton: CRC Press, 2017, pp. 733–771. URL:
+- P. Farrell, N. Rotundo, D. H. Doan, M. Kantner, J. Fuhrmann, and T. Koprucki, "Numerical methods for drift-diffusion models", in Handbook of optoelectronic device modeling and simulation: Lasers, modulators, photodetectors, solar cells, and numerical methods, vol. 2, J. Piprek, Ed. Boca Raton: CRC Press, 2017, pp. 733–771.
 
 =#
 
@@ -117,7 +117,7 @@ using VoronoiFVM, GridVisualize,ExtendableGrids
 
 function main(;n=10,Plotter=nothing,verbose=false, unknown_storage=:sparse,dim=2,tend=5, dt=0.2)
     n=[101,21,5]
-    X=collect(range(0,1,length=n[dim]))
+    X=collect(range(0.0,1,length=n[dim]))
     if dim==1
         grid=simplexgrid(X)
         Γ_where_T_equal_1=[2]
@@ -142,7 +142,7 @@ function main(;n=10,Plotter=nothing,verbose=false, unknown_storage=:sparse,dim=2
 	f[2]=u[2,1]-u[2,2]
     end
     
-    r(u1,u2)= u1-0.1*u2;
+    r(u1,u2)= u1-0.1*u2
     
     function reaction(f,u,node)
 	f[1]= r(u[1],u[2])
@@ -153,7 +153,7 @@ function main(;n=10,Plotter=nothing,verbose=false, unknown_storage=:sparse,dim=2
     function source(f,node)
 	f[1]=1.0
     end
-
+    
     physics=VoronoiFVM.Physics(num_species=2,
 	                       flux=flux,
 	                       storage=storage,
@@ -164,25 +164,25 @@ function main(;n=10,Plotter=nothing,verbose=false, unknown_storage=:sparse,dim=2
     enable_species!(system,1,[1])
     enable_species!(system,2,[1])
     boundary_dirichlet!(system,2,2,0.0);
-
+    
     
     inival=unknowns(system,inival=0.0)
-
+    
     u=solve(inival,system)
-
+    
     vis=GridVisualizer(Plotter=Plotter,layout=(1,2),resolution=(600,300),fignumber=1)
     scalarplot!(vis[1,1],grid,u[1,:],flimits=(0,1.5),title="u_1")
     scalarplot!(vis[1,2],grid,u[2,:],flimits=(0,1.5),title="u_2",show=true)
-
+    
     """
-    The `integrate` method of `VoronoiFVM`  provides a possibility to calculate
-    the volume integral of a function of a solution as described above. 
-    It returns a `num_specie` x `num_regions` matrix of the integrals 
-    of the function of the unknowns over the different subdomains (here, we have only one):
+        The `integrate` method of `VoronoiFVM`  provides a possibility to calculate
+        the volume integral of a function of a solution as described above. 
+        It returns a `num_specie` x `num_regions` matrix of the integrals 
+        of the function of the unknowns over the different subdomains (here, we have only one):
     """
     
     """
-    Amount of u_1 and u_2 in the domain aka integral over identity storage function:
+        Amount of u_1 and u_2 in the domain aka integral over identity storage function:
     """
     U=integrate(system,storage,u)
 
@@ -195,47 +195,45 @@ function main(;n=10,Plotter=nothing,verbose=false, unknown_storage=:sparse,dim=2
     Amount of  reaction per unit time:
     """
     R=integrate(system,reaction,u)
-
+    
 
     tf=VoronoiFVM.TestFunctionFactory(system)
     T=testfunction(tf,Γ_where_T_equal_0,Γ_where_T_equal_1)
     
     I=integrate(system,T,u)
-
     
-    t0=0.0; 
-
-    control=fixed_timesteps!(VoronoiFVM.NewtonControl(),dt);
+    
+    t0=0.0
+    
+    control=fixed_timesteps!(VoronoiFVM.NewtonControl(),dt)
 
     tsol=solve(inival,system,[t0,tend],control=control)
 
-
-    vis1=GridVisualizer(Plotter=Plotter,layout=(1,2),resolution=(600,300),fignumber=4);
-
+    
+    vis1=GridVisualizer(Plotter=Plotter,layout=(1,2),resolution=(600,300),fignumber=4)
+    
     for i=1:length(tsol)
         sol=tsol[i]
         scalarplot!(vis1[1,1],grid,sol[1,:],flimits=(0,1.5),clear=true)
         scalarplot!(vis1[1,2],grid,sol[2,:],flimits=(0,1.5),show=true)
     end
-
+    
     outflow_rate=Float64[]
     for i=2:length(tsol)
 	ofr=integrate(system,T,tsol[i],tsol[i-1],tsol.t[i]-tsol.t[i-1])
   	push!(outflow_rate,ofr[2])
     end
-
-    vis2=GridVisualizer(Plotter=Plotter,layout=(1,1),resolution=(600,300),fignumber=2);
+    
+    vis2=GridVisualizer(Plotter=Plotter,layout=(1,1),resolution=(600,300),fignumber=2)
     scalarplot!(vis2[1,1],[0,tend],-[I[2],I[2]],label="stationary",clear=true)
     scalarplot!(vis2[1,1],tsol.t[2:end],-outflow_rate,label="transient",show=true)
-
+    
     all_outflow=0.0
     for i=1:length(tsol)-1
 	all_outflow-=outflow_rate[i]*(tsol.t[i+1]-tsol.t[i])
     end
-    all_outflow
-
+    
     Uend=integrate(system,storage,tsol[end])
-
     
     F[1] ≈ R[1]  ? true : return false
     isapprox(I[1],0.0,atol=1.0e-16)  ? true  : return false
