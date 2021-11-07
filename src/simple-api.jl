847217c34980c8c232cc85572bb27ea9145ce4b0
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2021-11-07 12:22:27 +0100

    add missing file to branch

---
+++ src/simple-api.jl
@@ -0,0 +1,64 @@
+
+struct BoundaryCondition
+    ispec::Int64
+    ibreg::Int64
+    bcfac::Float64
+    bcval::Float64
+end
+
+
+function boundary_conditions!(sys,bcond)
+    for bc ∈ bcond
+        sys.boundary_factors[bc.ispec,bc.ibreg]=bc.bcfac
+        sys.boundary_values[bc.ispec,bc.ibreg]=bc.bcval
+    end
+end
+
+
+
+
+function System(grid;
+              num_species=0,
+              flux::Tflux=nothing,
+              reaction::Treaction=nothing,
+              bc=[]
+              ) where {Tflux, Treaction}
+    myflux= isnothing(flux) ? VoronoiFVM.nofunc : (y,u,edge)-> (y.=flux(u,edge))
+    myreaction= isnothing(reaction) ? VoronoiFVM.nofunc : (y,u,node)-> (y.=reaction(u,node))
+    physics=VoronoiFVM.Physics(flux=myflux, reaction =myreaction)
+    sys=VoronoiFVM.System(grid,physics)
+    allregions=collect(1:num_cellregions(grid))
+    for ispec=1:num_species
+        enable_species!(sys,ispec,allregions)
+    end
+    boundary_conditions!(sys,bc)
+    sys
+end
+
+
+function VoronoiFVM.solve(sys::VoronoiFVM.System; inival=0, kwargs...)
+    if isa(inival,Number)
+        inival=unknowns(sys,inival=inival)
+    elseif  !VoronoiFVM.isunknownsof(inival,sys)
+        @error "wrong type of inival: $(typeof(inival))"
+    end
+
+    control=VoronoiFVM.NewtonControl()
+    for k ∈ kwargs
+        try
+            setproperty!(control,k[1],k[2])
+        catch e
+            msg="solve: now such keyword: $(k[1])"
+            @error msg
+            println("Possible keywords & default values:")
+            println(control)
+            rethrow(e)
+        end
+    end
+    solve(inival,sys; control=control)
+end
+
+
+NeumannBC(;species=nothing, boundary=nothing, value=0)=BoundaryCondition(species,boundary,0,value)
+RobinBC(;species=nothing, boundary=nothing, factor=0,value=0)=BoundaryCondition(species,boundary,factor,value)
+DirichletBC(;species=nothing, boundary=nothing, value=0)=BoundaryCondition(species,boundary,VoronoiFVM.Dirichlet,value)
