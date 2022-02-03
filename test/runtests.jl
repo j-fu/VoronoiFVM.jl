db1e7a78209d8fe15aa7802d921b17f8f8ee2ef1
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2022-02-03 17:08:30 +0100

    run notebooks as notebooks for testing Before things were quite brittle - it happened that prerequisites for a particular test were no ready due to unintended re-sorting of cells.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using Test
+using Pluto
 # Activate assembly loop allocation checking
 # as default.
 ENV["VORONOIFVM_CHECK_ALLOCS"]="true"
@@ -29,6 +30,34 @@ function run_tests_from_directory(testdir,prefix)
     end
 end
 
+function testnotebook(input)
+    # de-markdown eventual cells with Pkg.develop and write
+    # to pluto-tmp.jl
+    # notebook=Pluto.load_notebook_nobackup(input)
+    # pkgcellfortest=findfirst(c->occursin("Pkg.develop",c.code),notebook.cells)
+    # if  pkgcellfortest!=nothing
+    #     # de-markdown pkg cell
+    #     notebook.cells[pkgcellfortest].code=replace(notebook.cells[pkgcellfortest].code,"md"=>"")
+    #     notebook.cells[pkgcellfortest].code=replace(notebook.cells[pkgcellfortest].code,"\"\"\""=>"")
+    #     notebook.cells[pkgcellfortest].code=replace(notebook.cells[pkgcellfortest].code,";"=>"")
+    #     @info "Pkg cell: $(pkgcellfortest)\n$(notebook.cells[pkgcellfortest].code)"
+    #     Pluto.save_notebook(notebook,"pluto-tmp.jl")
+    #     input="pluto-tmp.jl"
+    #     sleep(1)
+    # end
+
+    # run notebook and check for cell errors
+    session = Pluto.ServerSession();
+    notebook = Pluto.SessionActions.open(session, input; run_async=false)
+    errored=false
+    for c in notebook.cells
+        if c.errored
+            errored=true
+            @error "Error in  $(c.cell_id): $(c.output.body[:msg])\n $(c.code)"
+        end
+    end
+    !errored
+end
 
 function run_all_tests()
 
@@ -47,7 +76,8 @@ function run_all_tests()
         begin
             for notebook in notebooks
                 @testset "$(notebook)" begin
-                    include(joinpath(@__DIR__,"..","pluto-examples",notebook))
+                    #            include(joinpath(@__DIR__,"..","pluto-examples",notebook))
+                    @test testnotebook(joinpath(@__DIR__,"..","pluto-examples",notebook))
                 end
             end
         end
