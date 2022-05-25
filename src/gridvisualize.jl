c3e2dd277694f5a0f605d2b9794ca06a3ee42558
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2022-05-25 10:13:50 +0200

    updated gridvisualize for x-t plot

--- src/gridvisualize.jl
+++ src/gridvisualize.jl
@@ -25,12 +25,26 @@ end
 
 
 
-function GridVisualize.scalarplot!(vis,sys::AbstractSystem, sol::TransientSolution; species=1, kwargs...)
+function GridVisualize.scalarplot!(vis,sys::AbstractSystem, sol::TransientSolution; species=1,tscale=:identity,tlabel="t", kwargs...)
     if !isnothing(vis)
         grid=sys.grid
         @assert dim_space(grid)==1
         f=vec(sol[species,:,:])
         coord=grid[Coordinates]
-        GridVisualize.scalarplot!(vis,simplexgrid(coord[1,:],sol.t),f; kwargs...)
+        X=view(coord,1,:)
+        T=sol.t
+        if tscale== :log
+            T=log10.(sol.t)
+            if sol.t[1]≈0.0
+                T[1]=log10(sol.t[2]/2)
+            end
+            if tlabel=="t"
+                tlabel="log10(t)"
+            end
+        end
+        (xmin,xmax)=extrema(X)
+        (tmin,tmax)=extrema(T)
+        xtaspect=0.7*(tmax-tmin)/(xmax-xmin)
+        GridVisualize.scalarplot!(vis,simplexgrid(X,T),f;aspect=1/xtaspect, ylabel=tlabel, kwargs...)
     end
 end
