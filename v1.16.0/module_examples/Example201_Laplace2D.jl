99e9aa3760e85d93b8c72edd4bd73e666a5f3c1a
Documenter.jl <documenter@juliadocs.github.io>
2023-12-16 14:36:59 +0000

    build based on fd65b4f

---
+++ v1.16.0/module_examples/Example201_Laplace2D.jl
@@ -0,0 +1,49 @@
+#=
+
+# 201: 2D Laplace equation 
+([source code](@__SOURCE_URL__))
+
+=#
+
+module Example201_Laplace2D
+
+using VoronoiFVM, ExtendableGrids
+using GridVisualize
+using LinearAlgebra
+## Flux function which describes the flux
+## between neighboring control volumes
+function g!(f, u, edge)
+    f[1] = u[1, 1] - u[1, 2]
+end
+
+function main(; Plotter = nothing, n = 5, is_linear = true, assembly = :edgewise)
+    nspecies = 1
+    ispec = 1
+    X = collect(0:(1.0 / n):1)
+    grid = VoronoiFVM.Grid(X, X)
+
+    physics = VoronoiFVM.Physics(; flux = g!)
+    sys = VoronoiFVM.System(grid, physics; is_linear = is_linear, assembly = assembly)
+    enable_species!(sys, ispec, [1])
+    boundary_dirichlet!(sys, ispec, 1, 0.0)
+    boundary_dirichlet!(sys, ispec, 3, 1.0)
+    solution = solve(sys; inival = 0)
+    nf = nodeflux(sys, solution)
+    vis = GridVisualizer(; Plotter = Plotter)
+    scalarplot!(vis, grid, solution[1, :]; clear = true, colormap = :summer)
+    vectorplot!(vis, grid, nf[:, 1, :]; clear = false, spacing = 0.1, vscale = 0.5)
+    reveal(vis)
+    return norm(solution) + norm(nf)
+end
+
+## Called by unit test
+
+using Test
+function runtests()
+    testval = 9.63318042491699
+
+    @test main(; assembly = :edgewise) ≈ testval &&
+          main(; assembly = :cellwise) ≈ testval
+end
+
+end
--- versions.js
+++ versions.js
@@ -1,5 +1,6 @@
 var DOC_VERSIONS = [
   "stable",
+  "v1.16",
   "v1.15",
   "v1.14",
   "v1.13",
@@ -32,5 +33,5 @@ var DOC_VERSIONS = [
   "v0.5",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v1.15.1";
+var DOCUMENTER_NEWEST = "v1.16.0";
 var DOCUMENTER_STABLE = "stable";
