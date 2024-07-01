a85f95b5f27e69b4a38dca30467cd516936d2907
Documenter.jl <documenter@juliadocs.github.io>
2024-07-01 22:40:06 +0000

    build based on 2907bcc

---
+++ v1.21.0/module_examples/Example103_ConvectionDiffusion1D.jl
@@ -0,0 +1,86 @@
+#=
+
+# 103: 1D Convection-diffusion equation
+([source code](@__SOURCE_URL__))
+
+Solve the equation
+
+```math
+\partial_t u -\nabla ( D \nabla u - v u) = 0
+```
+in $\Omega=(0,1)$ with homogeneous Neumann boundary condition
+at $x=0$ and outflow boundary condition at $x=1$.
+=#
+
+module Example103_ConvectionDiffusion1D
+using Printf
+using VoronoiFVM
+using ExtendableGrids
+using GridVisualize
+
+## Bernoulli function used in the exponential fitting discretization
+function bernoulli(x)
+    if abs(x) < nextfloat(eps(typeof(x)))
+        return 1
+    end
+    return x / (exp(x) - 1)
+end
+
+function exponential_flux!(f, u, edge, data)
+    vh = project(edge, data.v)
+    Bplus = data.D * bernoulli(vh / data.D)
+    Bminus = data.D * bernoulli(-vh / data.D)
+    f[1] = Bminus * u[1, 1] - Bplus * u[1, 2]
+end
+
+function outflow!(f, u, node, data)
+    if node.region == 2
+        f[1] = data.v[1] * u[1]
+    end
+end
+
+function main(; n = 10, Plotter = nothing, D = 0.01, v = 1.0, tend = 100)
+
+    ## Create a one-dimensional discretization
+    h = 1.0 / n
+    grid = simplexgrid(0:h:1)
+
+    data = (v = [v], D = D)
+
+    sys = VoronoiFVM.System(grid,
+                            VoronoiFVM.Physics(; flux = exponential_flux!, data = data,
+                                               breaction = outflow!))
+
+    ## Add species 1 to region 1
+    enable_species!(sys, 1, [1])
+
+    ## Set boundary conditions
+    boundary_neumann!(sys, 1, 1, 0.0)
+
+    ## Create a solution array
+    inival = unknowns(sys)
+    inival[1, :] .= map(x -> 1 - 2x, grid)
+
+    ## Transient solution of the problem
+    control = VoronoiFVM.NewtonControl()
+    control.Δt = 0.01 * h
+    control.Δt_min = 0.01 * h
+    control.Δt_max = 0.1 * tend
+    tsol = solve(sys; inival, times = [0, tend], control)
+
+    vis = GridVisualizer(; Plotter = Plotter)
+    for i = 1:length(tsol.t)
+        scalarplot!(vis[1, 1], grid, tsol[1, :, i]; flimits = (0, 1),
+                    title = "t=$(tsol.t[i])", show = true)
+        sleep(0.01)
+    end
+    tsol
+end
+
+using Test
+function runtests()
+    tsol = main()
+    @test maximum(tsol) <= 1.0 && maximum(tsol.u[end]) < 1.0e-20
+end
+
+end
---
+++ v1.21.0/module_examples/Example207_NonlinearPoisson2D.jl
@@ -0,0 +1,83 @@
+# # 207: 2D Nonlinear Poisson equation
+# ([source code](@__SOURCE_URL__))
+
+module Example207_NonlinearPoisson2D
+
+using Printf
+using VoronoiFVM
+using ExtendableGrids
+using ExtendableSparse
+using GridVisualize
+using LinearSolve
+using ILUZero
+
+function main(; n = 10, Plotter = nothing, verbose = false, unknown_storage = :sparse,
+              method_linear = nothing, assembly = :edgewise,
+              precon_linear = A -> VoronoiFVM.Identity())
+    h = 1.0 / convert(Float64, n)
+    X = collect(0.0:h:1.0)
+    Y = collect(0.0:h:1.0)
+
+    grid = simplexgrid(X, Y)
+
+    eps = 1.0e-2
+
+    physics = VoronoiFVM.Physics(; reaction = function (f, u, node)
+                                     f[1] = u[1]^2
+                                 end, flux = function (f, u, edge)
+                                     f[1] = eps * (u[1, 1]^2 - u[1, 2]^2)
+                                 end, source = function (f, node)
+                                     x1 = node[1] - 0.5
+                                     x2 = node[2] - 0.5
+                                     f[1] = exp(-20.0 * (x1^2 + x2^2))
+                                 end, storage = function (f, u, node)
+                                     f[1] = u[1]
+                                 end)
+    sys = VoronoiFVM.System(grid, physics; unknown_storage, assembly = assembly)
+    enable_species!(sys, 1, [1])
+
+    boundary_dirichlet!(sys, 1, 2, 0.1)
+    boundary_dirichlet!(sys, 1, 4, 0.1)
+
+    inival = unknowns(sys)
+    inival .= 0.5
+
+    control = VoronoiFVM.NewtonControl()
+    control.verbose = verbose
+    control.reltol_linear = 1.0e-5
+    control.method_linear = method_linear
+    control.precon_linear = precon_linear
+    tstep = 0.01
+    time = 0.0
+    u15 = 0
+    p = GridVisualizer(; Plotter = Plotter)
+    while time < 1.0
+        time = time + tstep
+        U = solve(sys; inival, control, tstep)
+        u15 = U[15]
+        inival .= U
+
+        scalarplot!(p[1, 1], grid, U[1, :]; Plotter = Plotter, clear = true, show = true)
+        tstep *= 1.0
+    end
+    return u15
+end
+
+using Test
+function runtests()
+    # test at once for iterative solution here
+    testval = 0.3554284760906605
+    @test main(; unknown_storage = :sparse, assembly = :edgewise) ≈ testval &&
+          main(; unknown_storage = :dense, assembly = :edgewise) ≈ testval &&
+          main(; unknown_storage = :sparse, method_linear = KrylovJL_CG(), precon_linear = ILUZeroPreconditioner,
+               assembly = :edgewise) ≈ testval &&
+          main(; unknown_storage = :dense, method_linear = KrylovJL_CG(), precon_linear = ILUZeroPreconditioner,
+               assembly = :edgewise) ≈ testval &&
+          main(; unknown_storage = :sparse, assembly = :cellwise) ≈ testval &&
+          main(; unknown_storage = :dense, assembly = :cellwise) ≈ testval &&
+          main(; unknown_storage = :sparse, method_linear = KrylovJL_CG(), precon_linear = ILUZeroPreconditioner,
+               assembly = :cellwise) ≈ testval &&
+          main(; unknown_storage = :dense, method_linear = KrylovJL_CG(), precon_linear = ILUZeroPreconditioner,
+               assembly = :cellwise) ≈ testval
+end
+end
