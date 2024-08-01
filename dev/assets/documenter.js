bafc0c6ad86cc4a3f02b44f0eadbe84db0ddbc6a
Documenter.jl <documenter@juliadocs.github.io>
2024-08-01 23:31:00 +0000

    build based on 465e902

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -35,7 +35,8 @@ window.MathJax = {
     "packages": [
       "base",
       "ams",
-      "autoload"
+      "autoload",
+      "mhchem"
     ],
     "inlineMath": [
       [
@@ -58,7 +59,7 @@ window.MathJax = {
 
 (function () {
     var script = document.createElement('script');
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-svg.js';
+    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3.2.2/es5/tex-svg-full.js';
     script.async = true;
     document.head.appendChild(script);
 })();
