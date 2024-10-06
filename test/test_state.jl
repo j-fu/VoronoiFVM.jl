be70d4b24fde19241d31e5eba0b9f1fd8f2d8a3a
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2024-10-07 00:20:58 +0200

    fix Recursive Array indexing in test_state.jl

--- test/test_state.jl
+++ test/test_state.jl
@@ -17,6 +17,7 @@ function main(; unknown_storage=:dense)
     sys=VoronoiFVM.System(g; flux, bcondition, species=[1], unknown_storage)
     sol1=solve(sys)
 
+    # Solution and solution with state shall be the same
     state=VoronoiFVM.SystemState(sys)
     sol2=solve!(state)
     @test sol1≈sol2
@@ -25,16 +26,18 @@ function main(; unknown_storage=:dense)
     fixed_timesteps!(control,0.025)
 
 
+    # Allow initial values as result of previous time evolution
     tsol1=solve(sys; inival=0.0, times=(0,0.1), control)
     tsol2=solve(sys; inival=tsol1.u[end], times=(0.1,0.2), control)
     tsol3=solve(sys; inival=0.0, times=[0.0,0.1,0.2], control)
-    @test tsol3[end]≈tsol2[end]
+    @test tsol3.u[end]≈tsol2.u[end]
 
+    # Solution and solution with state shall be the same
     xsol1=solve!(state; inival=0.0, times=(0,0.1), control)
     xsol2=solve!(state; inival=tsol1.u[end], times=(0.1,0.2), control)
     xsol3=solve!(state; inival=0.0, times=[0.0,0.1,0.2], control)
-    @test xsol3[end]≈xsol2[end]
-    @test xsol3[end]≈tsol3[end]
+    @test xsol3.u[end]≈xsol2.u[end]
+    @test xsol3.u[end]≈tsol3.u[end]
 
 
 
